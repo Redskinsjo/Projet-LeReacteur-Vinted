03d782297769722d6ac92d31c1cb60384fdf850d
Jonathan Carnos <Jonathan.carnos@gmail.com>
2020-11-15 19:13:25 +0100

    use of cloudinary

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const formidable = require("express-formidable");
-const mongoose = require("mongoose");
-const cloudinary = require("cloudinary").v2;
-require("dotenv").config();
-const cors = require("cors");
+const express = require('express');
+const formidable = require('express-formidable');
+const mongoose = require('mongoose');
+const cloudinary = require('cloudinary').v2;
+require('dotenv').config();
+const cors = require('cors');
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
@@ -15,22 +15,22 @@ const app = express();
 app.use(formidable());
 app.use(cors());
 
-// Routes
-const userRoutes = require("./routes/user");
-app.use(userRoutes);
-const offerRoutes = require("./routes/offer");
-app.use(offerRoutes);
-
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-app.all("*", (req, res) => {
+// Routes
+const userRoutes = require('./routes/user');
+app.use(userRoutes);
+const offerRoutes = require('./routes/offer');
+app.use(offerRoutes);
+
+app.all('*', (req, res) => {
   res.status(400).json({ error: { message: "This route doesn't exist." } });
 });
 
 app.listen(process.env.PORT, () => {
-  console.log("Server started...");
+  console.log('Server started...');
 });
