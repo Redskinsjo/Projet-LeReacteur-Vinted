5c8b8455def5155f5d3c9da88606c6fee35f307d
Jonathan Carnos <Jonathan.carnos@gmail.com>
2020-11-15 21:26:01 +0100

    use of cloudinary

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(formidable());
 app.use(cors());
 
 cloudinary.config({
-  cloud_name: 'xde822mdsy',
+  cloud_name: 'de822mdsy',
   api_key: '683325958423579',
   api_secret: 'Ijex7t-hawHHXD9uX10L0f3Myso',
 });
