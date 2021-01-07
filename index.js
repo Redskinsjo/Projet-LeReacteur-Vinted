d9b5ee0a30e0c331d282acb8b059d9456116753c
Jonathan Carnos <Jonathan.carnos@gmail.com>
2021-01-07 18:05:02 +0100

    change db from local

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const formidable = require('express-formidable');
-const mongoose = require('mongoose');
-const cloudinary = require('cloudinary').v2;
-require('dotenv').config();
-const cors = require('cors');
+const express = require("express");
+const formidable = require("express-formidable");
+const mongoose = require("mongoose");
+const cloudinary = require("cloudinary").v2;
+require("dotenv").config();
+const cors = require("cors");
 
 // Connection to the DB
-mongoose.connect("mongodb://localhost/test"/*process.env.MONGODB_URI*/, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
 });
 
 // Initialization of the server
@@ -18,23 +18,23 @@ app.use(formidable());
 app.use(cors());
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 // Routes
-const userRoutes = require('./routes/user');
+const userRoutes = require("./routes/user");
 app.use(userRoutes);
-const offerRoutes = require('./routes/offer');
+const offerRoutes = require("./routes/offer");
 app.use(offerRoutes);
-const paymentRoutes = require('./routes/payment');
+const paymentRoutes = require("./routes/payment");
 app.use(paymentRoutes);
 
-app.all('*', (req, res) => {
-  res.status(400).json({ error: { message: "This route doesn't exist." } });
+app.all("*", (req, res) => {
+    res.status(400).json({ error: { message: "This route doesn't exist." } });
 });
 
 app.listen(process.env.PORT, () => {
-  console.log('Server started... PORT', process.env.PORT);
+    console.log("Server started... PORT", process.env.PORT);
 });
